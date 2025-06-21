078da70e9cec8201f91f00f2a2bab05ab701b199
André <andregsdantas@gmail.com>
2025-06-21 15:02:35 -0300

    add: criação da pasta mobile e tela de login

---
+++ mobile/app/login.tsx
@@ -0,0 +1,61 @@
+import React from "react";
+import { View, StyleSheet, Image, TextInput, TouchableHighlight, Text } from "react-native";
+
+export default function Login() {
+    return (
+        <View style={styles.bg}>
+            <View style={styles.container}>
+                <Image style={styles.logo} source={require('../assets/images/logotipo.png')} />
+                <TextInput style={styles.input} placeholder="CPF" />
+                <TextInput style={styles.input} placeholder="Senha" secureTextEntry={true} />
+                <TouchableHighlight style={styles.botao}>
+                    <Text style={styles.textoBotao}>ENTRAR</Text>
+                </TouchableHighlight>
+            </View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    bg: {
+        backgroundColor: '#FFFFFF',
+        height: '100%',
+        width: '100%',
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    container: {
+        width: '100%',
+        maxWidth: 500,
+        height: '100%',
+        backgroundColor: '#FFFFFF',
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 32
+    },
+    logo: {
+        width: '100%',
+        height: 'undefined',
+        aspectRatio: 1103/230,
+        marginBottom: 16
+    },
+    input: {
+        borderWidth: 1,
+        marginBottom: 16,
+        width: '100%',
+        padding: 8,
+        borderRadius: 32,
+    },
+    botao: {
+        width: '100%',
+        backgroundColor: '#F1D145',
+        padding: 8,
+        borderRadius: 32
+    },
+    textoBotao: {
+        textAlign: 'center',
+        fontWeight: 'bold'
+    }
+})
