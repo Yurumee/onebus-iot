282d41983f06d08f39f4ac08351940d6856110d8
Ana Alice <analicess.cc@gmail.com>
2025-06-25 18:42:06 -0300

    feat: modelo Motorista criado

--- backend/models/Motorista.py
+++ backend/models/Motorista.py
@@ -1,8 +1,12 @@
 from config import db
-from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import mapped_column, Mapped
 from sqlalchemy import String, Integer
 
 class Motorista(db.Model):
     __tablename__ = 'Motorista'
 
-    
+    CNH: Mapped[int] = mapped_column(Integer, primary_key=True, unique=True)
+    CPF: Mapped[str] = mapped_column(String(11), unique=True, nullable=False)
+    nomeCompleto: Mapped[str] = mapped_column(String(80), nullable=False)
+    senha: Mapped[str] = mapped_column(String(20), nullable=False)
+
