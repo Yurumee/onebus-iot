0c25dc23fce5d22255c025d1ce4dfcd0f98d061d
Ana Alice <analicess.cc@gmail.com>
2025-08-01 00:18:28 -0300

    refactor: padronização nas rotas com seus devidos metodos
    delete: import json em cidadao não necessario
    
    refactor: rota /signin mudado para /login (erro de nomeclatura)
    
    feat: paginas retornam render_template apontando para o frontend (apenas esqueleto, não completamente funcional)
    
    feat: rota para associar motorista a um carro agora recebe dados do front ao invés de dados mock
    
    feat: rota update de cidadao
    
    feat: rota delete de cidadao

--- backend/routes/viewCidadao.py
+++ backend/routes/viewCidadao.py
@@ -1,54 +1,186 @@
-import json
 from config import app, db
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request, jsonify, render_template
 from models.cidadao import Cidadao
 
 view_cidadao = Blueprint('view_cidadao', __name__)
 
-@view_cidadao.route('/')
+@view_cidadao.route('/', methods=['GET', 'POST'])
 def post_new_cidadao():
     """
-    Rota para cadastrar um cidadão mockado no banco de dados.
+    Rota para cadastrar um cidadão no banco de dados.
 
     Método:
-        GET
+        GET, POST
+        get: exibir pagina
+        post: cadastro dos dados
 
     Retorno:
         - status, message e dados do cidadão.
         - status error se o cidadão já existe.
     """
-    cpf = '789.456.123-00'
-    nome = 'Alice Mock'
-    senhaTop = 'senhaforte123'
+    if request.method == 'POST':
 
-    cidadao_existente = Cidadao.query.filter_by(cpf=cpf).first()
-    if cidadao_existente:
+        data = request.get_json()
+        cpf_cidadao = data.get('cidadao-cpf')
+        nome_cidadao = data.get('cidadao-nome')
+        senha = data.get('senha')
+
+        # cpf = '789.456.123-00'
+        # nome = 'Alice Mock'
+        # senha = 'senhaforte123'
+        try:
+            cidadao = Cidadao.query.filter_by(cpf=cpf_cidadao).first()
+        except Exception as e:
+            return jsonify({
+                "status":"error",
+                "message":f"{str(e)}"
+            }), 400
+
+        if cidadao:
+            return jsonify({
+                "status": "error",
+                "message": "Cidadão já cadastrado.",
+                "cpf": cpf_cidadao,
+                "nome": cidadao.nome_completo
+            }), 409
+
+        new_cidadao = Cidadao(
+            cpf=cpf_cidadao,
+            nome_completo=nome_cidadao,
+            senha=senha,
+            tipo_usuario='cidadao'
+        )
+
+        db.session.add(new_cidadao)
+        db.session.commit()
+
         return jsonify({
-            "status": "error",
-            "message": "Cidadão já cadastrado.",
-            "cpf": cpf,
-            "nome": cidadao_existente.nome_completo
-        }), 409
+            "status": "success",
+            "message": "Cidadão cadastrado com sucesso.",
+            "cpf": cpf_cidadao,
+            "nome": nome_cidadao
+        }), 201
+    
+    return render_template('pagina_cadastro.html'), 200
 
-    MOCKCidadao = Cidadao(
-        cpf=cpf,
-        nome_completo=nome,
-        senha=senhaTop,
-        tipo_usuario='cidadao'
-    )
 
-    db.session.add(MOCKCidadao)
-    db.session.commit()
+@view_cidadao.route('/alterar-cidadao', methods=['GET', 'PATCH'])
+def edit_cidadao():
+    """
+    Rota para editar um cidadão.
 
-    return jsonify({
-        "status": "success",
-        "message": "Cidadão cadastrado com sucesso.",
-        "cpf": cpf,
-        "nome": nome
-    }), 201
+    Métodos:
+        GET, PATCH
 
-@app.route('/signin', methods=['GET', 'POST'])
-def singin():
+    Parâmetros esperados (via formulário):
+        - cpf: cpf do cidadão a ser alterado
+        - nome: novo nome do cidadão
+
+    Retorno:
+        - status, message e dados do cidadão.
+        - status error se usuário não existir ou senha estiver incorreta.
+    """
+
+    if request.method == 'PATCH':
+
+        data = request.get_json()
+        cpf_cidadao = data.get('cidadao-cpf')
+
+        try:
+            cidadao = Cidadao.query.filter_by(cpf=cpf_cidadao).first()
+        except Exception as e:
+            return jsonify({
+                "status":"error",
+                "message":f"{str(e)}"
+            })
+        
+        if cidadao:
+            new_nome = data.get('cidadao-nome')
+
+            if new_nome != cidadao.nome_completo:
+                try:
+                    cidadao.nome_completo = new_nome
+                    db.session.commit()
+
+                except Exception as e:
+                    return jsonify({
+                        "status":"error",
+                        "message":f"{str(e)}"
+                    })
+                
+                return jsonify({
+                    "status":"success",
+                    "message":"update realizado com sucesso"
+                }), 200
+            
+            else:
+                return jsonify({
+                    "status":"error",
+                    "message":"o nome precisa ser diferente do atual"
+                }), 400
+        else:
+            return jsonify({
+                "status":"error",
+                "message":"cidadao nao encontrado"
+            }), 404
+        
+    return render_template('pagina_editar_cidadao.html')
+            
+
+@view_cidadao.route('/excluir-cidadao', methods=['GET', 'DELETE'])
+def delete_cidadao():
+    """
+    Rota para deletar um cidadão.
+
+    Métodos:
+        GET, DELETE
+
+    Parâmetros esperados:
+        - cpf: cpf do cidadão
+
+    Retorno:
+        - status, message.
+        - status error se usuário não existir
+    """
+
+    if request.method == 'DELETE':
+        data = request.get_json()
+        cpf_cidadao = data.get('cidadao-cpf')
+
+        try:
+            cidadao = Cidadao.query.filter_by(cpf=cpf_cidadao).first()
+        except Exception as e:
+            return jsonify({
+                "status":"error",
+                "message":f"{str(e)}"
+            }), 400
+        
+        if cidadao:
+            try:
+                Cidadao.query.filter_by(cpf=cpf_cidadao).delete()
+                db.session.commit()
+                return jsonify({
+                    "status":"success",
+                    "message":"cidadao deletado"
+                }), 204
+            
+            except Exception as e:
+                return jsonify({
+                "status":"error",
+                "message":f"{str(e)}"
+            }), 400
+
+        else:
+            return jsonify({
+                "status":"error",
+                "message":"cidadao nao encontrado"
+            }), 404
+    
+    return render_template('pagina_deletar_cidadao.html'), 302
+        
+
+@view_cidadao.route('/login', methods=['GET', 'POST'])
+def login():
     """
     Rota para autenticação de cidadão.
 
@@ -63,29 +195,35 @@ def singin():
         - status, message e dados do cidadão.
         - status error se usuário não existir ou senha estiver incorreta.
     """
-    dadosRequest = json.loads(request.data)
-    cpf = dadosRequest['cpf']
-    password = dadosRequest['senha']
 
-    user = Cidadao.query.filter_by(cpf=cpf).first()
+    if request.method == 'POST':
+    
+        dadosRequest = request.get_json()
+        cpf = dadosRequest.get('cpf')
+        password = dadosRequest.get('senha')
 
-    if user:
-        if user.senha == password:
-            return jsonify({
-                "status": "success",
-                "message": "Autenticação realizada com sucesso.",
-                "cpf": cpf,
-                "nome": user.nome_completo
-            }), 200
-        else:
-            return jsonify({
-                "status": "error",
-                "message": "Senha incorreta.",
-                "cpf": cpf
-            }), 401
+        user = Cidadao.query.filter_by(cpf=cpf).first()
 
-    return jsonify({
-        "status": "error",
-        "message": "Usuário não encontrado.",
-        "cpf": cpf
-    }), 404
+        if user:
+            if user.senha == password:
+                return jsonify({
+                    "status": "success",
+                    "message": "Autenticação realizada com sucesso.",
+                    "cpf": cpf,
+                    "nome": user.nome_completo
+                }), 200
+            
+            else:
+                return jsonify({
+                    "status": "error",
+                    "message": "Senha incorreta.",
+                    "cpf": cpf
+                }), 401
+
+        return jsonify({
+            "status": "error",
+            "message": "Usuário não encontrado.",
+            "cpf": cpf
+        }), 404
+
+    return render_template('pagina_login.html'), 302
--- backend/routes/viewMotorista.py
+++ backend/routes/viewMotorista.py
@@ -11,53 +11,56 @@ def post_new_motorista():
     Rota para cadastrar um motorista mockado no banco de dados.
     """
 
-    data = request.get_json() # pega todos os dados passados pelo body
-    cnh = data.get('motorista-cnh') 
-    cpf = data.get('motorista-cpf') 
-    nome = data.get('motorista-nome') 
-    senha = data.get('senha') 
-    tipo_usuario = data.get('tipo-usuario') 
-    carroPlaca = data.get('carro-placa')
+    if request.method == 'POST':
+        data = request.get_json() # pega todos os dados passados pelo body
+        cnh_motorista = data.get('motorista-cnh') 
+        cpf_motorista = data.get('motorista-cpf') 
+        nome_motorista = data.get('motorista-nome') 
+        senha = data.get('senha') 
+        tipo_usuario = data.get('tipo-usuario') 
+        carro_placa = data.get('carro-placa')
 
-    # cnh = 9876543219
-    # cpf = '123.456.789-00'
-    # nome = 'Alice Mock 2'
-    # senha = 'senhamuitoforte123'
-    # tipo_usuario = 'motorista'
-    # carroPlaca = '4N4L1C3'
-    # carroPlaca = None
+        # cnh = 9876543219
+        # cpf = '123.456.789-00'
+        # nome = 'Alice Mock 2'
+        # senha = 'senhamuitoforte123'
+        # tipo_usuario = 'motorista'
+        # carroPlaca = '4N4L1C3'
+        # carroPlaca = None
 
-    motorista_existente = Motorista.query.filter_by(cpf=cpf).first()
-    cnh_existente = Motorista.query.filter_by(cnh=cnh).first()
-    if motorista_existente or cnh_existente:
+        motorista_existente = Motorista.query.filter_by(cpf=cpf_motorista).first()
+        cnh_existente = Motorista.query.filter_by(cnh=cnh_motorista).first()
+        if motorista_existente or cnh_existente:
+            return jsonify({
+                "status": "error",
+                "message": "Motorista já cadastrado.",
+                "cpf": cpf_motorista,
+                # "nome": motorista_existente.nome_completo
+            }), 409
+
+
+        new_motorista = Motorista(
+            cnh=cnh_motorista,
+            cpf=cpf_motorista,
+            nome_completo=nome_motorista,
+            carro_placa=carro_placa,
+            senha=senha,
+            tipo_usuario=tipo_usuario,
+        )
+
+        db.session.add(new_motorista)
+        db.session.commit()
+
         return jsonify({
-            "status": "error",
-            "message": "Motorista já cadastrado.",
-            "cpf": cpf,
-            # "nome": motorista_existente.nome_completo
-        }), 409
+            "status": "success",
+            "message": "Motorista cadastrado com sucesso.",
+            "cpf": cpf_motorista,
+            "nome": nome_motorista
+        }), 201
     
+    return render_template('pagina_cadastro_motorista.html'), 302
 
-    MOCKtorista = Motorista(
-        cnh=cnh,
-        cpf=cpf,
-        nome_completo=nome,
-        carro_placa=carroPlaca,
-        senha=senha,
-        tipo_usuario=tipo_usuario,
-    )
-
-    db.session.add(MOCKtorista)
-    db.session.commit()
-
-    return jsonify({
-        "status": "success",
-        "message": "Motorista cadastrado com sucesso.",
-        "cpf": cpf,
-        "nome": nome
-    }), 201
-
-@view_motorista.route('/excluir-motorista', methods=['GET', 'POST'])
+@view_motorista.route('/excluir-motorista', methods=['GET', 'DELETE'])
 def delete_motorista():
     """
     Rota para deletar um motorista específico com o cpf informado
@@ -68,7 +71,7 @@ def delete_motorista():
     Retorno:
         Página indicando motorista deletado
     """
-    if request.method == 'POST':
+    if request.method == 'DELETE':
         cpf_motorista = request.form.get('motorista-cpf')
         motorista = Motorista.query.filter_by(cpf=cpf_motorista).first()
 
@@ -79,7 +82,7 @@ def delete_motorista():
                 return jsonify({
                     "status":"success",
                     "message":"motorista deletado com sucesso"
-                }), 200
+                }), 204
             
             except Exception as e:
                 return jsonify({
@@ -120,7 +123,7 @@ def edit_motorista():
             if new_nome != motorista.nome_completo:
                 try:
                     motorista.nome_completo = new_nome
-                    # db.session.commit()
+                    db.session.commit()
 
                 except Exception as e:
                     return jsonify({
@@ -132,15 +135,16 @@ def edit_motorista():
             if new_placa != motorista.carro_placa or new_placa != None:
                 try:
                     carro_desejado = Carro.query.filter_by(placa=new_placa).first()
+
                     if carro_desejado:
                         carro_desejado.motorista_cnh.append(motorista)
+                        db.session.commit()
 
                     else:
                         return jsonify({
                             "status":"not found",
                             "message":"esta placa não existe"
                         })
-                    # db.session.commit()
 
                 except Exception as e:
                     return jsonify({
@@ -149,7 +153,7 @@ def edit_motorista():
                         "error":f"{str(e)}"
                     })
             
-            db.session.commit()
+            # db.session.commit()
             return jsonify({
                 "status":"success",
                 "message":"update realizado com sucesso"
@@ -162,47 +166,54 @@ def edit_motorista():
             })
 
     else:
-        return render_template('motoristas.html')
+        return render_template('motoristas.html'), 302
 
 @view_motorista.route('/motorista-carro', methods=['GET', 'POST'])
 def register_carro():
     """
-    Rota para associar um carro mockado a um motorista mockado.
+    Rota para associar um carro a um motorista mockado.
     """
-    cnh_motorista = 9876543210
-    placa_motorista = 'S54NT05'
+    # cnh_motorista = 9876543210
+    # placa_motorista = 'S54NT05'
 
-    motorista = Motorista.query.filter_by(cnh=cnh_motorista).first()
-    carro_desejado = Carro.query.filter_by(placa=placa_motorista).first()
+    if request.method == 'POST':
+        data = request.get_json()
+        cnh_motorista = data.get('motorista-cnh')
+        placa_motorista = data.get('placa')
 
-    if not motorista:
+        motorista = Motorista.query.filter_by(cnh=cnh_motorista).first()
+        carro_desejado = Carro.query.filter_by(placa=placa_motorista).first()
+
+        if not motorista:
+            return jsonify({
+                "status": "error",
+                "message": "Motorista não encontrado.",
+                "cnh": cnh_motorista
+            }), 404
+
+        if not carro_desejado:
+            return jsonify({
+                "status": "error",
+                "message": "Carro não encontrado.",
+                "placa": placa_motorista
+            }), 404
+
+        if motorista.carro_placa == placa_motorista:
+            return jsonify({
+                "status": "warning",
+                "message": "Placa já associada ao motorista.",
+                "cnh": cnh_motorista,
+                "placa": placa_motorista
+            }), 200
+
+        carro_desejado.motorista_cnh.append(motorista)
+        db.session.commit()
+
         return jsonify({
-            "status": "error",
-            "message": "Motorista não encontrado.",
-            "cnh": cnh_motorista
-        }), 404
-
-    if not carro_desejado:
-        return jsonify({
-            "status": "error",
-            "message": "Carro não encontrado.",
-            "placa": placa_motorista
-        }), 404
-
-    if motorista.carro_placa == placa_motorista:
-        return jsonify({
-            "status": "warning",
-            "message": "Placa já associada ao motorista.",
+            "status": "success",
+            "message": "Carro associado ao motorista com sucesso.",
             "cnh": cnh_motorista,
             "placa": placa_motorista
         }), 200
-
-    carro_desejado.motorista_cnh.append(motorista)
-    db.session.commit()
-
-    return jsonify({
-        "status": "success",
-        "message": "Carro associado ao motorista com sucesso.",
-        "cnh": cnh_motorista,
-        "placa": placa_motorista
-    }), 200
+    
+    return render_template('pagina_associar_carro.html'), 302
