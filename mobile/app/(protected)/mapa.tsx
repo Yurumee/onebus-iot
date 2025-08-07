303dd6c56f48e69469e9524c422906c88812694c
André <andregsdantas@gmail.com>
2025-08-06 23:32:55 -0300

    feat: navegabilidade para a tela do mapa

--- mobile/app/(protected)/mapa.tsx
+++ mobile/app/(protected)/mapa.tsx
@@ -10,6 +10,9 @@ export default function Mapa() {
 
   return (
     <View style={styles.container}>
+        <TouchableHighlight underlayColor={'#FFFFFF'} style={styles.botaoHome} onPress={() => router.replace('/',{})}>
+          <Image style={styles.home} source={require('../../assets/images/onebus-home.png')} />
+        </TouchableHighlight>
         <Map
           style={StyleSheet.absoluteFill}
           initialRegion={{
@@ -19,12 +22,12 @@ export default function Mapa() {
             longitudeDelta: 0.005
           }}
         >
-          <Marker coordinate={{latitude: -6.263355590562354, longitude: -36.51601747810453}}/>
+          <Marker coordinate={{latitude: -6.263355590562354, longitude: -36.51601747810453}}/> {/* Teste */}
           <Polyline
             coordinates={[{latitude: -6.261005625698471, longitude: -36.51917365752614}, {latitude: -6.2598641808347, longitude: -36.52014332697427}]}
             strokeColor="#000"
             strokeWidth={6}
-          />
+          /> {/* Teste */}
         </Map>
     </View>
   );
@@ -33,5 +36,22 @@ export default function Mapa() {
 const styles = StyleSheet.create({
     container: {
         flex: 1
+    },
+    botaoHome: {
+        position: 'absolute',
+        zIndex: 1,
+        left: 16,
+        top: 32,
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: 64,
+        height: 64,
+        backgroundColor: '#FFFFFF',
+        borderRadius: 64
+    },
+    home: {
+        width: 50,
+        height: 33
     }
 });
