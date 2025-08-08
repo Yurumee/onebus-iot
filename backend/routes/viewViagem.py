6ceb6e5eceaef640da2aa28b153a42a0533589c3
Ana Alice <analicess.cc@gmail.com>
2025-08-08 00:14:02 -0300

    feat: rota post ponto viagem

--- backend/routes/viewViagem.py
+++ backend/routes/viewViagem.py
@@ -1,5 +1,5 @@
 from config import db, app
-from flask import Blueprint, jsonify, render_template
+from flask import Blueprint, jsonify, render_template, request
 # from sqlalchemy import select
 from datetime import datetime
 from models.trajeto import Trajeto
@@ -8,9 +8,9 @@ from models.pontoViagem import PontoViagem
 view_viagem = Blueprint('view_viagem', __name__)
 
 @view_viagem.route('/', methods=['GET', 'POST'])
-def post_ponto_viagem():
+def post_new_ponto_viagem():
     """
-    Rota para cadastrar um ponto de viagem mockado no banco de dados.
+    Rota para cadastrar um ponto de viagem no banco de dados.
 
     Métodos:
         GET, POST
@@ -19,35 +19,38 @@ def post_ponto_viagem():
         - status, message e dados do trajeto.
         - status error se ocorrer exceção.
     """
-    horarioComeco = datetime(year=2025, month=7, day=4, hour=13, minute=30, second=0)
-    
-    MOCKPontoV = PontoViagem(
-        latitude_ponto='10.000001',
-        longitude_ponto='-11.000002',
-        data=horarioComeco.date(),
-        hora=horarioComeco.time()
-        # motoristaResp=1234567890,      # Descomente se o campo existir no modelo
-        # idEmbarcado='abc123'           # Descomente se o campo existir no modelo
-    )
 
-    try:
-        db.session.add(MOCKPontoV)
-        db.session.commit()
-        return jsonify({
-            "status": "success",
-            "message": "Trajeto inserido com sucesso.",
-            "trajeto": {
-                "latitude_ponto": MOCKPontoV.latitude_ponto,
-                "longitude_ponto": MOCKPontoV.longitude_ponto,
-                "data": MOCKPontoV.data,
-                "hora": MOCKPontoV.hora,
-                # "horario_estimado": str(MOCKPontoV.horario_estimado)
-                # "motoristaResp": MOCKTrajeto.motoristaResp,   # Inclua se existir
-                # "idEmbarcado": MOCKTrajeto.idEmbarcado        # Inclua se existir
-            }
-        }), 201
-    except Exception as e:
-        return jsonify({
-            "status": "error",
-            "message": f"Erro ao inserir trajeto: {str(e)}"
-        }), 400
+    if request.method == 'POST':
+        data = request.get_json()
+        latitude_ponto = data.get('latitude')
+        longitude_ponto = data.get('longitude')
+        datahora = datetime.strptime(data.get('datahora-estimado'), "%Y-%m-%dT%H:%M:%S")
+
+        # horarioComeco = datetime(year=2025, month=7, day=4, hour=13, minute=30, second=0)
+
+        new_ponto_viagem = PontoViagem(
+            latitude_ponto=latitude_ponto,
+            longitude_ponto=longitude_ponto,
+            data=datahora.date(),
+            hora=datahora.time()
+        )
+
+        try:
+            db.session.add(new_ponto_viagem)
+            db.session.commit()
+            return jsonify({
+                "status": "success",
+                "message": "Trajeto inserido com sucesso.",
+                "trajeto": {
+                    "latitude_ponto": new_ponto_viagem.latitude_ponto,
+                    "longitude_ponto": new_ponto_viagem.longitude_ponto,
+                    "data": new_ponto_viagem.data,
+                    "hora": new_ponto_viagem.hora
+                }
+            }), 201
+        
+        except Exception as e:
+            return jsonify({
+                "status": "error",
+                "message": f"Erro ao inserir novo ponto de viagem: {str(e)}"
+            }), 400
