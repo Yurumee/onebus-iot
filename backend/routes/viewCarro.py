c1ec4c7171cf0ba4ddf68d2936656783ee4623c8
Ana Alice <analicess.cc@gmail.com>
2025-08-07 22:35:27 -0300

    feat: rota post carro feat: rota delete carro

--- backend/routes/viewCarro.py
+++ backend/routes/viewCarro.py
@@ -3,50 +3,67 @@ from flask import Blueprint, jsonify, request, render_template
 from models.carro import Carro
 from models.motorista import Motorista
 from models.trajeto import Trajeto
+from models.pontoViagem import PontoViagem
 
 view_carro = Blueprint('view_carro', __name__)
 
 @view_carro.route('/', methods=['GET', 'POST'])
 def post_new_car():
     """
-    Rota para cadastrar carros de exemplo no banco de dados.
+    Rota para cadastrar carros no banco de dados.
 
     Método:
         GET, POST
 
     Retorno:
-        Uma mensagem indicando que os carros foram cadastrados ou já existem.
+        carros cadastrados no banco de dados
     
-    Observação:
-        Esta rota insere dois carros mockados no banco de dados para fins de teste.
     """
-    carros_mock = [
-        {"placa": "4N4L1C3", "tipo": "Carro"},
-        {"placa": "S54NT05", "tipo": "Van"}
-    ]
-    cadastrados = []
-    ja_existentes = []
-
-    for carro in carros_mock:
-        placa = carro["placa"]
-        tipo = carro["tipo"]
-        existe = Carro.query.filter_by(placa=placa).first()
-        if existe:
-            ja_existentes.append(placa)
+    if request.method == 'POST':
+        data = request.get_json()
+        placa_carro = data.get('placa-carro')
+        tipo_carro = data.get('tipo-veiculo')
+        
+        try:
+            carro_existente = Carro.query.filter_by(placa=placa_carro).first()
+        except Exception as e:
+            return jsonify({
+                "status":"error",
+                "message":f"{str(e)}"
+            }), 400
+        
+        if carro_existente:
+            return jsonify({
+                "status":"error",
+                "message":"carro ja cadastrado"
+            }), 409
+        
         else:
-            novo_carro = Carro(placa=placa, tipo_veiculo=tipo)
-            db.session.add(novo_carro)
-            cadastrados.append(placa)
+            novo_carro = Carro(
+                placa=placa_carro,
+                tipo_veiculo=tipo_carro
+            )
 
-    db.session.commit()
+            if data.get('longitude-atual') and data.get('latitude-atual'):
+                novo_carro.longitude_atual = data.get('longitude-atual')
+                novo_carro.latitude_atual = data.get('latitude-atual')
+            
+            try:
+                db.session.add(novo_carro)
+                db.session.commit()
 
-    mensagem = ""
-    if cadastrados:
-        mensagem += f"Carros cadastrados: {', '.join(cadastrados)}.\n"
-    if ja_existentes:
-        mensagem += f"Carros já existentes: {', '.join(ja_existentes)}."
-
-    return mensagem.strip()
+            except Exception as e:
+                return jsonify({
+                "status":"error",
+                "message":f"erro ao inserir no banco de dados: {str(e)}"
+                }), 400
+            
+            return jsonify({
+                "status":"success",
+                "message":"carro cadastrado com sucesso"
+                }), 201
+    
+    return render_template('pagina_inserir_carro.html'), 302
 
 @view_carro.route('/alterar-carro', methods=['GET', 'PATCH'])
 def edit_carro():
@@ -97,3 +114,47 @@ def edit_carro():
             }), 404
     
     return render_template('pagina_editar_motorista.html'), 302
+
+@view_carro.route('/excluir-carro', methods=['GET', 'DELETE'])
+def delete_carro():
+    
+    if request.method == 'DELETE':
+        # delete from motoristas
+        # delete from trajetos
+        # delete from pontos de trajeto
+
+        data = request.get_json()
+        placa_carro = data.get('placa-carro')
+
+        try:
+            carro_desejado = Carro.query.filter_by(placa=placa_carro).first()
+        except Exception as e:
+            return jsonify({
+                "status":"error",
+                "message":f"{str(e)}"
+            }), 400
+
+        if carro_desejado:
+            try:
+                db.session.query(Trajeto).where(Trajeto.carro_placa == placa_carro).delete()
+                db.session.query(PontoViagem).where(PontoViagem.carro_placa == placa_carro).delete()
+                Carro.query.filter_by(placa=placa_carro).delete()
+            
+            except Exception as e:
+                return jsonify({
+                "status":"error",
+                "message":f"houve um erro {str(e)}"
+            }), 400
+
+            return jsonify({
+                "status":"deleted",
+                "message":"carro deletado com sucesso"
+            }), 200
+
+        else:
+            return jsonify({
+                "status":"not found",
+                "message":"carro nao encontrado"
+            }), 404
+    
+    return render_template('pagina_deletar_carro.html'), 302
