c4c18e7a5e032bf2f016bf069a0f4e34f4252187
Ana Alice <analicess.cc@gmail.com>
2025-08-12 00:17:49 -0300

    feat: returns json da rota de viagem padronizados

--- backend/routes/viewViagem.py
+++ backend/routes/viewViagem.py
@@ -26,11 +26,12 @@ def post_new_ponto_trajeto():
 
         try:
             trajeto_desejado = Trajeto.query.filter_by(id_trajeto=id_trajeto).first()
+
         except Exception as e:
             return jsonify({
                 "status":"error",
                 "message":f"{str(e)}"
-            }), 400
+            }), 500
         
         if trajeto_desejado:
             
@@ -53,26 +54,19 @@ def post_new_ponto_trajeto():
             except Exception as e:
                 return jsonify({
                     "status": "error",
-                    "message": f"Erro ao inserir novo ponto de trajeto: {str(e)}"
-                }), 400
+                    "message": f"{str(e)}"
+                }), 500
             
             return jsonify({
-                    "status": "success",
-                    "message": "Trajeto inserido com sucesso.",
-                    "trajeto": {
-                        "latitude_ponto": new_ponto_trajeto.latitude,
-                        "longitude_ponto": new_ponto_trajeto.longitude,
-                        "tipo": new_ponto_trajeto.tipo_ponto
-                    }
+                    "status": "created"
                 }), 201
 
         else:
             return jsonify({
-                "status":"not found",
-                "message":"o trajeto desejado nao existe"
+                "status":"not found"
             }), 404
         
-    return render_template('pagina_cadastrar_ponto_trajeto.html'), 302
+    # return render_template('pagina_cadastrar_ponto_trajeto.html'), 302
 
 @view_viagem.route('/alterar-ponto', methods=['GET', 'PATCH'])
 def edit_ponto_trajeto():
@@ -88,8 +82,16 @@ def edit_ponto_trajeto():
     if request.method == 'PATCH':
         data = request.get_json()
         id_ponto = data.get('id-ponto-trajeto')
-        ponto_desejado = PontoTrajeto.query.filter_by(id_ponto_traj=id_ponto).first()
 
+        try:
+            ponto_desejado = PontoTrajeto.query.filter_by(id_ponto_traj=id_ponto).first()
+
+        except Exception as e:
+            return jsonify({
+                "status": "error",
+                "message": f"{str(e)}"
+            }), 500
+        
         if ponto_desejado:
             latitude_ponto = data.get('latitude')
             longitude_ponto = data.get('longitude')
@@ -97,43 +99,68 @@ def edit_ponto_trajeto():
             tipo_ponto = data.get('tipo-ponto')
 
             if latitude_ponto != ponto_desejado.latitude and latitude_ponto != None:
-                ponto_desejado.latitude = latitude_ponto
-                db.session.commit()
+                try:
+                    ponto_desejado.latitude = latitude_ponto
+                    db.session.commit()
+                
+                except Exception as e:
+                    return jsonify({
+                        "status": "error",
+                        "message": f"{str(e)}"
+                    }), 500
 
             if longitude_ponto != ponto_desejado.longitude and longitude_ponto != None:
-                ponto_desejado.longitude = longitude_ponto
-                db.session.commit()
-
-            if tipo_ponto != ponto_desejado.tipo_ponto and tipo_ponto != None:
-                ponto_desejado.tipo_ponto = tipo_ponto
-                db.session.commit()
-
-            if new_trajeto != ponto_desejado.trajeto_id and new_trajeto != None:
-                trajeto_desejado = Trajeto.query.filter_by(id_trajeto=new_trajeto).first()
-
-                if trajeto_desejado:
-                    trajeto_desejado.trajeto_ponto.append(ponto_desejado)
+                try:
+                    ponto_desejado.longitude = longitude_ponto
                     db.session.commit()
-
-                else:
+                
+                except Exception as e:
                     return jsonify({
-                        "status":"not found",
-                        "message":"trajeto nao encontrado"
-                    }), 404
+                        "status": "error",
+                        "message": f"{str(e)}"
+                    }), 500
             
+            if tipo_ponto != ponto_desejado.tipo_ponto and tipo_ponto != None:
+                try:
+                    ponto_desejado.tipo_ponto = tipo_ponto
+                    db.session.commit()
+                
+                except Exception as e:
+                    return jsonify({
+                        "status": "error",
+                        "message": f"{str(e)}"
+                    }), 500
+                
+            if new_trajeto != ponto_desejado.trajeto_id and new_trajeto != None:
+                try:
+                    trajeto_desejado = Trajeto.query.filter_by(id_trajeto=new_trajeto).first()
+
+                    if trajeto_desejado:
+                        trajeto_desejado.trajeto_ponto.append(ponto_desejado)
+                        db.session.commit()
+
+                    else:
+                        return jsonify({
+                            "status":"not found"
+                        }), 404
+                    
+                except Exception as e:
+                    return jsonify({
+                        "status": "error",
+                        "message": f"{str(e)}"
+                    }), 500
+                
             return jsonify({
-                "status":"success",
-                "message":"updates realizados com sucesso"
-            }), 200
+                "status":"updated"
+            }), 204
         
         else:
             return jsonify({
-                "status":"not found",
-                "message":"ponto nao encontrado"
+                "status":"not found"
             }), 404
 
 
-    return render_template('editar_ponto_trajeto.html'), 302
+    # return render_template('editar_ponto_trajeto.html'), 302
 
 @view_viagem.route('/rota-especifica', methods=['GET', 'POST'])
 def get_pontos_trajeto():
@@ -149,15 +176,26 @@ def get_pontos_trajeto():
 
     data = request.get_json()
     trajeto_id = data.get('trajeto-id')
-    trajeto_desejado = Trajeto.query.filter_by(id_trajeto=trajeto_id).first()
-
+    try:
+        trajeto_desejado = Trajeto.query.filter_by(id_trajeto=trajeto_id).first()
+    except Exception as e:
+        return jsonify({
+            "status": "error",
+            "message": f"{str(e)}"
+        }), 500
+    
     if trajeto_desejado:
-        # pontos_trajeto = db.session.query(PontoTrajeto, Trajeto).join(Trajeto, Trajeto.id_trajeto == trajeto_id).all()
-        pontos_trajeto = db.session.query(PontoTrajeto).where(PontoTrajeto.trajeto_id == trajeto_id).all()
-
-        print(pontos_trajeto)
-
+        try:
+            pontos_trajeto = db.session.query(PontoTrajeto).where(PontoTrajeto.trajeto_id == trajeto_id).all()
+        
+        except Exception as e:
+            return jsonify({
+                "status": "error",
+                "message": f"{str(e)}"
+            }), 500
+    
         resposta_json = {}
+    
         for ponto in pontos_trajeto:
             resposta_json[ponto.id_ponto_traj] = {"id_trajeto": ponto.trajeto_id,
                                                   "latitude": ponto.latitude,
@@ -167,14 +205,12 @@ def get_pontos_trajeto():
 
         return jsonify({
             "status": "success",
-            "message": "pontos de trajeto encontrados.",
-            "trajetos": resposta_json,
+            "data": resposta_json,
         }), 200
 
     else:
         return jsonify({
-            "status":"not found",
-            "message":"trajeto nao encontrado"
+            "status":"not found"
         }), 404
 
 
@@ -192,15 +228,15 @@ def get_all_pontos_trajeto():
         Página mostrando todos os pontos
     """
 
-    # data = request.get_json()
-    # trajeto_id = data.get('trajeto-id')
-    pontos_trajeto = PontoTrajeto.query.all()
+    try:
+        pontos_trajeto = PontoTrajeto.query.all()
+    except Exception as e:
+        return jsonify({
+            "status": "error",
+            "message": f"{str(e)}"
+        }), 500
 
     if pontos_trajeto:
-        # pontos_trajeto = db.session.query(PontoTrajeto, Trajeto).join(Trajeto, Trajeto.id_trajeto == trajeto_id).all()
-        # pontos_trajeto = db.session.query(PontoTrajeto).where(PontoTrajeto.trajeto_id == trajeto_id).all()
-
-        print(pontos_trajeto)
 
         resposta_json = {}
         for ponto in pontos_trajeto:
@@ -212,14 +248,12 @@ def get_all_pontos_trajeto():
 
         return jsonify({
             "status": "success",
-            "message": "pontos de trajeto encontrados.",
-            "trajetos": resposta_json,
+            "data": resposta_json,
         }), 200
 
     else:
         return jsonify({
             "status":"not found",
-            "message":"trajeto nao encontrado"
         }), 404
 
 
@@ -239,24 +273,35 @@ def delete_pontos_trajeto():
 
     data = request.get_json()
     id_ponto = data.get('id-ponto')
-    ponto_trajeto = PontoTrajeto.query.filter_by(id_ponto_traj=id_ponto).first()
+
+    try:
+        ponto_trajeto = PontoTrajeto.query.filter_by(id_ponto_traj=id_ponto).first()
+
+    except Exception as e:
+        return jsonify({
+            "status": "error",
+            "message": f"{str(e)}"
+        }), 500
 
     if ponto_trajeto:
-        db.session.query(PontoTrajeto).filter(PontoTrajeto.id_ponto_traj==id_ponto).delete()
-        db.session.commit()
+        try:
+            db.session.query(PontoTrajeto).filter(PontoTrajeto.id_ponto_traj==id_ponto).delete()
+            db.session.commit()
+
+        except Exception as e:
+            return jsonify({
+                "status": "error",
+                "message": f"{str(e)}"
+            }), 500
 
         return jsonify({
-            "status": "success",
-            "message": "ponto de trajeto deletado.",
-            # "trajetos": resposta_json,
-        }), 200
+            "status": "deleted"
+        }), 204
 
     else:
         return jsonify({
             "status":"not found",
-            "message":"ponto nao encontrado"
         }), 404
-
 
     # return render_template('pagina_pontos_trajeto.html'), 302
 
@@ -274,24 +319,33 @@ def delete_all_pontos_trajeto():
 
     data = request.get_json()
     id_trajeto = data.get('id-trajeto')
-    pontos_trajeto = PontoTrajeto.query.filter_by(trajeto_id=id_trajeto).all()
-    print(pontos_trajeto)
+
+    try:
+        pontos_trajeto = PontoTrajeto.query.filter_by(trajeto_id=id_trajeto).all()
+    
+    except Exception as e:
+        return jsonify({
+            "status": "error",
+            "message": f"{str(e)}"
+        }), 500
 
     if pontos_trajeto:
-        db.session.query(PontoTrajeto).where(PontoTrajeto.trajeto_id == id_trajeto).delete()
-        db.session.commit()
+        try:
+            db.session.query(PontoTrajeto).where(PontoTrajeto.trajeto_id == id_trajeto).delete()
+            db.session.commit()
+        except Exception as e:
+            return jsonify({
+                "status": "error",
+                "message": f"{str(e)}"
+            }), 500
 
         return jsonify({
-            "status": "success",
-            "message": "pontos de trajeto deletado.",
-            # "trajetos": resposta_json,
-        }), 200
+            "status": "deleted",
+        }), 204
 
     else:
         return jsonify({
-            "status":"not found",
-            "message":"pontos nao encontrados"
+            "status":"not found"
         }), 404
-
 
     # return render_template('pagina_pontos_trajeto.html'), 302
